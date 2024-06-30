b665f02c962202c1f3c7bf0dc16d2270415cd5b2
Breno Dionisio <brnodionisio23@gmail.com>
2024-06-30 14:44:24 -0300

    Implementando lógica do jogo(calculando o ganhador da rodada)

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
 public class Main {
-
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -12,19 +10,37 @@ public class Main {
         System.out.println("BEM VINDO AO PAR OU IMPAR");
         System.out.println(" ");
 
-        while(novaRodada.equals("s")) {
+        while (novaRodada.equals("s")) {
             System.out.println("Escolha: par ou impar");
             escolhaDoJogador = sc.next();
 
-            if(escolhaDoJogador.equals("par")) {
+            if (escolhaDoJogador.equals("par")) {
                 escolhaDoComputador = "impar";
                 ladoDoComputador(escolhaDoComputador);
-            }
-            else {
+            } else {
                 escolhaDoComputador = "par";
-                    ladoDoComputador(escolhaDoComputador);
+                ladoDoComputador(escolhaDoComputador);
             }
 
+            System.out.println("Digite um número entre 0 e 9");
+            valorDoJogador = sc.nextInt();
+
+            valorDoComputador = (int) (Math.random() * 10);
+            System.out.println("Computador: " + valorDoComputador);
+
+            if((valorDoJogador + valorDoComputador) % 2 == 0){
+                if (escolhaDoJogador.equals("par")){
+                    System.out.println("Você venceu!");
+                } else{
+                    System.out.println("O computador venceu!");
+                }
+            } else {
+                if (escolhaDoJogador.equals("impar")){
+                    System.out.println("Você venceu!");
+                } else{
+                    System.out.println("O computador venceu!");
+                }
+            }
 
             System.out.print("Deseja realizar mais uma rodada? (s / n): ");
             novaRodada = sc.next().toLowerCase();
@@ -36,8 +52,10 @@ public class Main {
     static String novaRodada;
     static String escolhaDoJogador;
     static String escolhaDoComputador;
+    static int valorDoJogador;
+    static int valorDoComputador;
 
-    public static void ladoDoComputador(String lado){
+    public static void ladoDoComputador(String lado) {
         System.out.println("Computador: " + lado + "!");
     }
 

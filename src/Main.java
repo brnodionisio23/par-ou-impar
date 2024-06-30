0f8fe2cc395cc809240a12ccd785b1d7bebb5c2e
Breno Dionisio <brnodionisio23@gmail.com>
2024-06-30 14:04:20 -0300

    Implementando lógica de lado (par ou impar)

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,38 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        String novaRodada = "s";
+        novaRodada = "s";
 
         System.out.println("BEM VINDO AO PAR OU IMPAR");
-        System.out.println("");
+        System.out.println(" ");
+
         while(novaRodada.equals("s")) {
-            System.out.println("teste");
-            System.out.print("Deseja realizar mais uma rodada? (s / n)");
+            System.out.println("Escolha: par ou impar");
+            escolhaDoJogador = sc.next();
+
+            if(escolhaDoJogador.equals("par")) {
+                escolhaDoComputador = "impar";
+            }
+            else {
+                escolhaDoComputador = "par";
+            }
+
+            System.out.println(escolhaDoComputador);
+
+            System.out.print("Deseja realizar mais uma rodada? (s / n): ");
             novaRodada = sc.next().toLowerCase();
         }
 
         sc.close();
     }
+
+    static String novaRodada;
+    static String escolhaDoJogador;
+    static String escolhaDoComputador;
 }

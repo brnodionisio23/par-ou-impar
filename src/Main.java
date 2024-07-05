c08654e444cdbfb687505aae1543ac6595e575fa
Breno Dionisio <brnodionisio23@gmail.com>
2024-07-04 21:55:23 -0300

    Implementando sistema de pontuação

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import game.Game;
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
-        game.choosenPlayersSide();
-        game.choosenPlayersValue();
+        game.match();
 
     }
 }
--- src/game/Game.java
+++ src/game/Game.java
@@ -8,11 +8,16 @@ import java.util.Scanner;
 public class Game {
     Player p1 = new Player();
     Cpu p2 = new Cpu();
+    int p1Victories = 0;
+    int p2Victories = 0;
 
     Scanner sc = new Scanner(System.in);
 
     public void match() {
-
+        choosenPlayersSide();
+        choosenPlayersValue();
+        score(p1.getSide(), p1.getValue(), p2.getValue());
+        System.out.println(p1Victories + " X " + p2Victories);
     }
 
     public void choosenPlayersSide() {
@@ -49,6 +54,24 @@ public class Game {
             System.out.println("Computador: " + p2.getValue());
             System.out.println(" ");
         }
+
+    }
+
+    public int[] score(String p1Side, int p1Value, int p2Value) {
+        p1Value = p1.getValue();
+        p2Value = p2.getValue();
+        p1Side = p1.getSide();
+
+        if ((p1Value + p2Value) % 2 == 0) {
+            if (p1Side.equals("par")) {
+                p1Victories++;
+                System.out.println("Você venceu!");
+            } else {
+                p2Victories++;
+                System.out.println("O computador venceu!");
+            }
+        }
+        return new int[]{p1Victories, p2Victories};
 
     }
 }
